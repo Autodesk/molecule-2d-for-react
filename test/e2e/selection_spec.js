160accbc1385fc30904255598e3cf83771d11297
Justin McCandless <justinjmccandless@gmail.com>
2016-09-07 16:22:13 -0700

    Selection e2e test tests multiple selection

--- test/e2e/selection_spec.js
+++ test/e2e/selection_spec.js
@@ -1,12 +1,23 @@
 module.exports = {
-  'Selection' : function (browser) {
+  'Selection Test': (browser) => {
     browser
       .url('http://localhost:4000')
       .waitForElementVisible('.nbmolviz2d svg', 1000, 'nbmolviz2s SVG element appears')
       .waitForElementVisible('g.node', 1000, 'Nodes are rendered in the SVG.')
       .click('g.node')
-      .pause(1000)
       .assert.elementPresent('g.node.selected', 'Clicking a node adds a selected class to it')
+      .click('g.node:last-child')
+      // Clicking a different node selects both of them
+      .elements('css selector', 'g.node.selected', (result) => {
+        browser.expect(result.value.length).to.equal(2);
+      })
+      .click('g.node')
+      // Clicking a selected node unselects it
+      .elements('css selector', 'g.node.selected', (result) => {
+        browser.expect(result.value.length).to.equal(1);
+      })
+      .click('g.node:last-child')
+      .assert.elementNotPresent('g.node.selected', 'Clicking all selected nodes unselects them all')
       .end();
-  }
+  },
 };
