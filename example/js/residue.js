05de8564811af44060708f8b52b970accbf2fbdc
Justin McCandless <justinjmccandless@gmail.com>
2016-10-18 09:44:42 -0700

    Switching between molecules in example works again, by offsetting link ids

--- example/js/residue.js
+++ example/js/residue.js
@@ -14,17 +14,17 @@ const residue = {
     {"id":998,"atom":"H"}
   ],
   links: [
-    { id: 0, "source":989,"target":990,"bond":1, strength: 1, distance: 30.0 },
-    { id: 1, "source":989,"target":998,"bond":1, strength: 1, distance: 30.0 },
-    { id: 2, "source":990,"target":991,"bond":1, strength: 1, distance: 30.0 },
-    { id: 3, "source":990,"target":993,"bond":1, strength: 1, distance: 30.0 },
-    { id: 4, "source":991,"target":992,"bond":2, strength: 1, distance: 30.0 },
-    { id: 5, "source":993,"target":994,"bond":1, strength: 1, distance: 30.0 },
-    { id: 6, "source":994,"target":995,"bond":1, strength: 1, distance: 30.0 },
-    { id: 7, "source":995,"target":997,"bond":1, strength: 1, distance: 30.0 },
-    { id: 8, "source":995,"target":996,"bond":2, strength: 1, distance: 30.0 },
-    { id: 9, "source":997,"target":1000,"bond":1, strength: 1, distance: 30.0 },
-    { id: 10, "source":997,"target":999,"bond":1, strength: 1, distance: 30.0 }
+    { id: 90, "source":989,"target":990,"bond":1, strength: 1, distance: 30.0 },
+    { id: 91, "source":989,"target":998,"bond":1, strength: 1, distance: 30.0 },
+    { id: 92, "source":990,"target":991,"bond":1, strength: 1, distance: 30.0 },
+    { id: 93, "source":990,"target":993,"bond":1, strength: 1, distance: 30.0 },
+    { id: 94, "source":991,"target":992,"bond":2, strength: 1, distance: 30.0 },
+    { id: 95, "source":993,"target":994,"bond":1, strength: 1, distance: 30.0 },
+    { id: 96, "source":994,"target":995,"bond":1, strength: 1, distance: 30.0 },
+    { id: 97, "source":995,"target":997,"bond":1, strength: 1, distance: 30.0 },
+    { id: 98, "source":995,"target":996,"bond":2, strength: 1, distance: 30.0 },
+    { id: 99, "source":997,"target":1000,"bond":1, strength: 1, distance: 30.0 },
+    { id: 100, "source":997,"target":999,"bond":1, strength: 1, distance: 30.0 }
   ],
 };
 
