0fd1ec4f2252b092a61288ce27d8465241679ae5
Justin McCandless <justinjmccandless@gmail.com>
2016-10-31 11:56:07 -0700

    Set up selenium downloader so travis will run it, but users of the project wont

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,9 +1,6 @@
-const fs = require('fs');
-const seleniumDownload = require('selenium-download');
-
 const BINPATH = './node_modules/nightwatch/bin/';
 
-const config = {
+module.exports = {
   src_folders: [
     'test/e2e', // Where you are storing your Nightwatch e2e/UAT tests
   ],
@@ -35,19 +32,3 @@ const config = {
     },
   },
 };
-
-/**
- * selenium-download does exactly what it's name suggests;
- * downloads (or updates) the version of Selenium (& chromedriver)
- * on your localhost where it will be used by Nightwatch.
- */
-fs.stat(`${BINPATH}selenium.jar`, (err, stat) => {
-  if (err || !stat || stat.size < 1) {
-    seleniumDownload.ensure(BINPATH, (error) => {
-      if (error) throw new Error(error);
-      console.log('✔ Selenium & Chromedriver downloaded to:', BINPATH);
-    });
-  }
-});
-
-module.exports = config;
